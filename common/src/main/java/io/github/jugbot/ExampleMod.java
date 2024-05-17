0ba35211b3c70e93cff3100baae6d5aa3bcd1f9f
Lucas Pollice <lp1935@nyu.edu>
2024-05-17 12:38:55 -0400

    init template

---
+++ common/src/main/java/io/github/jugbot/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.jugbot;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "example_mod";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
