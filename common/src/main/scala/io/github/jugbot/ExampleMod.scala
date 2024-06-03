3daef44c53ee59cdd3fb0ee7937c8e29038d5514
Lucas Pollice <lp1935@nyu.edu>
2024-06-03 11:34:15 -0400

    adopt mod id settlements

--- common/src/main/scala/io/github/jugbot/ExampleMod.scala
+++ common/src/main/scala/io/github/jugbot/ExampleMod.scala
@@ -1,7 +1,7 @@
 package io.github.jugbot;
 
 object ExampleMod {
-    final val MOD_ID = "example_mod";
+    final val MOD_ID = "settlements";
 
     def init() = {
         // Write common init code here.
