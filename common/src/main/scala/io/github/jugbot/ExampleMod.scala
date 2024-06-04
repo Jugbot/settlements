060b5880cdbc722c0f2c5268672aefb14fd14b36
Lucas Pollice <lp1935@nyu.edu>
2024-06-03 21:20:58 -0400

    qs

--- common/src/main/scala/io/github/jugbot/ExampleMod.scala
+++ common/src/main/scala/io/github/jugbot/ExampleMod.scala
@@ -1,9 +1,13 @@
 package io.github.jugbot;
 
+import org.apache.logging.log4j.LogManager
+
 object ExampleMod {
     final val MOD_ID = "settlements";
 
+    private val LOGGER = LogManager.getLogger
+
     def init() = {
-        // Write common init code here.
+        LOGGER.info("Hello World!")
     }
 }
