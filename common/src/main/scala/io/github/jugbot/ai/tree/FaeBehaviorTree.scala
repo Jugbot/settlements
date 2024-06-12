9e9e340f73a40ff86720ea602e93ced39a3b6b13
Lucas Pollice <lp1935@nyu.edu>
2024-06-12 01:47:25 -0400

    simple behavior tree implementation

---
+++ common/src/main/scala/io/github/jugbot/ai/tree/FaeBehaviorTree.scala
@@ -0,0 +1,31 @@
+package io.github.jugbot.ai.tree
+
+import io.github.jugbot.ai.SequenceNode
+import io.github.jugbot.ai.Node
+import io.github.jugbot.ai.ActionNode
+import io.github.jugbot.ai.SelectorNode
+
+enum FaeBehavior {
+  case unknown extends FaeBehavior
+  case unimplemented extends FaeBehavior
+  case sleep extends FaeBehavior
+  case is_tired extends FaeBehavior
+  case has_valid_bed extends FaeBehavior
+  case claim_bed extends FaeBehavior
+}
+
+object FaeBehaviorTree {
+  val root: Node[FaeBehavior] = SequenceNode()
+  val survival = SequenceNode()
+  val sleep = SequenceNode(
+    ActionNode(FaeBehavior.is_tired),
+    SelectorNode(
+      ActionNode(FaeBehavior.has_valid_bed),
+      claimBed
+    ),
+    goToBlock,
+    ActionNode(FaeBehavior.sleep)
+  )
+  val goToBlock = ActionNode(FaeBehavior.unimplemented)
+  val claimBed = ActionNode(FaeBehavior.claim_bed)
+}
--- common/src/main/scala/io/github/jugbot/entity/FaeEntity.scala
+++ common/src/main/scala/io/github/jugbot/entity/FaeEntity.scala
@@ -15,6 +15,23 @@ import net.minecraft.world.level.Level
 import java.lang
 import java.util.function.Supplier
 import java.util as ju
+import io.github.jugbot.ai.state
+import io.github.jugbot.ai.Node
+import io.github.jugbot.ai.ActionNode
+import io.github.jugbot.ai.Failure
+import io.github.jugbot.ai.Success
+import io.github.jugbot.ai.tree.FaeBehavior
+import io.github.jugbot.ai.Status
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.core.BlockPos
+import net.minecraft.nbt.NbtUtils
+import io.github.jugbot.ai.tree.FaeBehaviorTree
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.entity.ai.village.poi.PoiTypes
+import net.minecraft.world.entity.ai.village.poi.PoiManager
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.tags.BlockTags
+import net.minecraft.world.level.block.BedBlock
 
 class FaeEntity(entityType: EntityType[? <: LivingEntity], world: Level) extends LivingEntity(entityType, world) {
 
@@ -37,9 +54,82 @@ class FaeEntity(entityType: EntityType[? <: LivingEntity], world: Level) extends
   override def attackable(): Boolean = false
 
   override def isEffectiveAi(): Boolean = false
+
+  override def baseTick(): Unit = {
+    super.baseTick()
+    state(
+      FaeEntity.behaviorTree,
+      (behavior: FaeBehavior) =>
+        behavior match {
+          case FaeBehavior.unknown => {
+            println("Encountered a behavior without an implementation!")
+            Failure
+          }
+          case FaeBehavior.unimplemented => {
+            println("Encountered unimplemented behavior, skipping.")
+            Success
+          }
+          case FaeBehavior.is_tired => {
+            if this.level().isNight() then Success else Failure
+          }
+          case FaeBehavior.has_valid_bed => {
+            bedPosition match {
+              case None => Failure
+              case Some(blockPos) => {
+                val blockState: BlockState = this.level().asInstanceOf[ServerLevel].getBlockState(blockPos);
+                if blockPos.closerToCenterThan(this.position(), 2.0) && blockState.is(BlockTags.BEDS) && blockState
+                    .getValue(BedBlock.OCCUPIED) == false
+                then Success
+                else Failure
+              }
+            }
+          }
+          case FaeBehavior.sleep => {
+            if this.bedPosition.isDefined then Success else Failure
+          }
+          case FaeBehavior.claim_bed => {
+            val poiManager = this.level().asInstanceOf[ServerLevel].getPoiManager()
+            // TODO: instead of finding beds within a distance we should keep record of all beds within the kingdom
+            val maybeHome = poiManager.findClosest(holder => holder.is(PoiTypes.HOME),
+                                                   this.blockPosition(),
+                                                   48,
+                                                   PoiManager.Occupancy.HAS_SPACE
+            )
+            if maybeHome.isPresent() then {
+              val takenPOI = poiManager.take(holder => holder.is(PoiTypes.HOME), (_, _) => true, maybeHome.get, 32)
+              bedPosition = if takenPOI.isPresent() then Some(takenPOI.get) else None
+              bedPosition match {
+                case Some(value) => Success
+                case None        => Failure
+              }
+            } else Failure
+          }
+        }
+    )
+  }
+
+  var bedPosition: Option[BlockPos] = Option.empty
+
+  override def addAdditionalSaveData(compoundTag: CompoundTag): Unit = {
+    super.addAdditionalSaveData(compoundTag);
+    bedPosition.foreach(pos => compoundTag.put(FaeEntity.BED_POSITION_NBT_KEY, NbtUtils.writeBlockPos(bedPosition.get)))
+  }
+
+  override def readAdditionalSaveData(compoundTag: CompoundTag): Unit = {
+    super.readAdditionalSaveData(compoundTag);
+    if compoundTag.contains(FaeEntity.BED_POSITION_NBT_KEY) then {
+      this.bedPosition = Some(
+        NbtUtils.readBlockPos(
+          compoundTag.getCompound(FaeEntity.BED_POSITION_NBT_KEY)
+        )
+      )
+    }
+  }
 }
 
 object FaeEntity {
+  val BED_POSITION_NBT_KEY = "bedPosition"
+
   final val TYPE: Supplier[EntityType[FaeEntity]] = Suppliers.memoize(() =>
     EntityType.Builder
       .of[FaeEntity](new FaeEntity(_, _), MobCategory.MISC)
@@ -51,6 +141,8 @@ object FaeEntity {
     LivingEntity
       .createLivingAttributes()
       .add(Attributes.MAX_HEALTH, 10.0)
-      .add(Attributes.MOVEMENT_SPEED, 0.2f);
+      .add(Attributes.MOVEMENT_SPEED, 0.2f)
   }
+
+  val behaviorTree: Node[FaeBehavior] = FaeBehaviorTree.root
 }
