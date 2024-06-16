35fad5d7642e3e643d2a33079c163a00c9d67fc6
Lucas Pollice <lp1935@nyu.edu>
2024-06-16 00:20:17 -0400

    testing enums with args serialization

--- common/src/test/scala/io/github/jugbot/BTSerialization.scala
+++ common/src/test/scala/io/github/jugbot/BTSerialization.scala
@@ -35,19 +35,38 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.annotation.targetName
 
-enum ExampleBehavior {
-  case eat extends ExampleBehavior
-  case sleep extends ExampleBehavior
-  case rave extends ExampleBehavior
-  case re_eat extends ExampleBehavior
+enum ExampleBehavior(args: String*) {
+  case unknown
+  case eat(food: String) extends ExampleBehavior(food)
+  case sleep
+  case rave
+  case re_eat(food: String) extends ExampleBehavior(food)
+}
+
+object ExampleBehavior {
+  def valueOf(jsonValue: String): ExampleBehavior = {
+    val tokens = jsonValue.split("""[\(\),]""").toList
+
+    tokens match {
+      case behaviorType :: args =>
+        behaviorType match {
+          case "eat"    => ExampleBehavior.eat(args(0))
+          case "re_eat" => ExampleBehavior.re_eat(args(0))
+          case "sleep"  => ExampleBehavior.sleep
+          case "rave"   => ExampleBehavior.rave
+          case _        => ExampleBehavior.unknown
+        }
+      case _ => ExampleBehavior.unknown
+    }
+  }
 }
 
 class BTSerialization extends AnyFunSuite with Matchers {
   val jsonFixture: String = """{
                               |  "sequence" : [ {
-                              |    "action" : "eat"
+                              |    "action" : "eat(bagel)"
                               |  }, {
-                              |    "action" : "re_eat"
+                              |    "action" : "re_eat(bagel)"
                               |  }, {
                               |    "selector" : [ {
                               |      "action" : "sleep"
@@ -56,10 +75,34 @@ class BTSerialization extends AnyFunSuite with Matchers {
                               |}""".stripMargin
 
   val treeFixture: SequenceNode[ExampleBehavior] = SequenceNode(
-    ActionNode(ExampleBehavior.eat),
-    ActionNode(ExampleBehavior.re_eat),
+    ActionNode(ExampleBehavior.eat("bagel")),
+    ActionNode(ExampleBehavior.re_eat("bagel")),
     SelectorNode(ActionNode(ExampleBehavior.sleep))
   )
+
+  test("serializes enum to json") {
+    val result: String = BTMapper.mapper.writeValueAsString(ExampleBehavior.eat("bagel"))
+
+    println(result)
+
+    result shouldEqual "\"eat(bagel)\""
+  }
+
+  test("deserializes unknown to enum") {
+    val result = BTMapper.mapper.readValue("\"whatever\"", classOf[ExampleBehavior])
+
+    println(result)
+
+    result shouldEqual ExampleBehavior.unknown
+  }
+
+  test("deserializes json to enum") {
+    val result = BTMapper.mapper.readValue("\"eat(bagel)\"", classOf[ExampleBehavior])
+
+    println(result)
+
+    result shouldEqual ExampleBehavior.eat("bagel")
+  }
 
   test("serializes to json") {
     val result = BTMapper.mapper.writeValueAsString(treeFixture)
