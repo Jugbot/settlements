bde691d3eee427c467e153a8441dddd15f8c3733
Lucas Pollice <lp1935@nyu.edu>
2024-07-30 23:02:31 -0400

    custom texture

--- common/src/main/scala/io/github/jugbot/screen/ShrineScreen.scala
+++ common/src/main/scala/io/github/jugbot/screen/ShrineScreen.scala
@@ -5,27 +5,27 @@ import io.github.jugbot.blockentity.ShrineBlockEntity
 import net.minecraft.client.GameNarrator
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.Screen
-import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.inventory.AnvilMenu
 
 class ShrineScreen(blockEntity: ShrineBlockEntity) extends Screen(GameNarrator.NO_TITLE) {
-  private val leftPos = (this.width - ShrineScreen.imageWidth) / 2
-  private val topPos = (this.height - ShrineScreen.imageHeight) / 2
+  private var leftPos = 0
+  private var topPos = 0
 
   override def init(): Unit = {
     super.init()
+    this.leftPos = (this.width - ShrineScreen.imageWidth) / 2
+    this.topPos = (this.height - ShrineScreen.imageHeight) / 2
   }
 
   override def render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float): Unit = {
     this.renderBackground(guiGraphics)
-    guiGraphics.blit(ShrineScreen.background, this.leftPos + 59, this.topPos + 20, 0, ShrineScreen.imageHeight, 110, 16)
+    guiGraphics.blit(ShrineScreen.background, this.leftPos, this.topPos, 0, 0, ShrineScreen.imageWidth, ShrineScreen.imageHeight)
     super.render(guiGraphics, mouseX, mouseY, partialTicks)
   }
 }
 
 object ShrineScreen {
-  private val imageWidth = 176
+  private val imageWidth = 248
   private val imageHeight = 166
-  private val background = new ResourceLocation("textures/gui/container/anvil.png");
+  private val background = new ResourceLocation(Mod.MOD_ID,"textures/gui/shrine.png");
 }
