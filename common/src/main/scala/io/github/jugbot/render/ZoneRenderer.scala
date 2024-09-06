658d82d2494b6b08fbd01c664f5741845a39615b
Lucas Pollice <lp1935@nyu.edu>
2024-09-05 20:42:38 -0400

    Only render when debug hitboxes is on

--- common/src/main/scala/io/github/jugbot/render/ZoneRenderer.scala
+++ common/src/main/scala/io/github/jugbot/render/ZoneRenderer.scala
@@ -1,13 +1,13 @@
 package io.github.jugbot.render
 
 import com.mojang.blaze3d.vertex.PoseStack
-import io.github.jugbot.entity.zone.ZoneType.Zoning
+import dev.architectury.utils.GameInstance
 import io.github.jugbot.entity.zone.{ZoneEntity, ZoneType}
 import net.minecraft.client.renderer.culling.Frustum
 import net.minecraft.client.renderer.entity.{EntityRenderer, EntityRendererProvider}
 import net.minecraft.client.renderer.{LevelRenderer, MultiBufferSource, RenderType}
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.level.levelgen.structure.Structure
 import net.minecraft.world.phys.shapes.Shapes
 
 case class RGBA(r: Float, g: Float, b: Float, a: Float)
@@ -20,7 +20,7 @@ class ZoneRenderer(context: EntityRendererProvider.Context)
   override def getTextureLocation(entity: ZoneEntity): ResourceLocation =
     null
 
-  override def shouldRender(entity: ZoneEntity, frustum: Frustum, d: Double, e: Double, f: Double): Boolean = true
+  override def shouldRender(entity: ZoneEntity, frustum: Frustum, d: Double, e: Double, f: Double): Boolean = GameInstance.getClient.getEntityRenderDispatcher.shouldRenderHitBoxes
 
   override def render(entity: ZoneEntity,
                       f: Float,
@@ -33,8 +33,7 @@ class ZoneRenderer(context: EntityRendererProvider.Context)
     val voxelShape = Shapes.create(bb)
     val color = entity.getZoneType match {
       case ZoneType.Settlement => RGBA(0.0f, 0.0f, 1.0f, 0.2f)
-      case ZoneType.Zoning     => RGBA(1.0f, 0.0f, 0.0f, 0.2f)
-      case ZoneType.Structure  => RGBA(0.0f, 1.0f, 0.0f, 0.2f)
+      case ZoneType.Zoning     => RGBA(0.5f, 0.5f, 1.0f, 0.2f)
       case _                   => RGBA(1.0f, 1.0f, 1.0f, 0.2f)
     }
     val center = bb.getCenter
@@ -67,5 +66,5 @@ class ZoneRenderer(context: EntityRendererProvider.Context)
       color.a
     )
 
-    super.render(entity, f, g, poseStack, multiBufferSource, i)
+    this.renderNameTag(entity, Component.literal(entity.getZoneType.toString), poseStack, multiBufferSource, i)
 }
